c142a2bf8ddc2c1bb75271d8e15494ae650c3636
AndyFM806 <leefernandez1245@gmail.com>
2025-01-08 21:35:10 -0500

    ejercicio

---
+++ ejercicio1.js
@@ -0,0 +1,7 @@
+//
+//Ejercicio 1: Imprimir los números del N al 1
+//Descripción: Pide al usuario un número N y muestra en consola los números desde N hasta 1 en orden descendente.
+let N = parseInt(prompt("Ingresa un numero"));
+for(let i; i<=N; i++){
+    alert(" "+ i);
+}
